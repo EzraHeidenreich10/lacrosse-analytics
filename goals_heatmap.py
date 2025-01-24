067986b60a0682fa074da15357c11e9c316da785
Ezra Heidenreich <edh5342@psu.edu>
2025-01-23 21:55:38 -0500

    vmin, vmax added

--- goals_heatmap.py
+++ goals_heatmap.py
@@ -3,13 +3,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-data = pd.read_csv('test_data.csv')
+data = pd.read_csv('heatmap_test_data.csv')
 
 row_labels = ['Top', 'Mid', 'Bottom']
 col_labels = ['L', 'Mid', 'R']
 
-
-sns.heatmap(data, cmap='Reds', cbar=True, yticklabels=row_labels, xticklabels=col_labels)
+sns.heatmap(data, vmin=0, vmax=50, cmap='Reds', cbar=True, yticklabels=row_labels, xticklabels=col_labels)
 
 plt.show()
     
