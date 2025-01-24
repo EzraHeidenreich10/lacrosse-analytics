341ded9b7846390fc3875e2d0be1159ed45fc966
Ezra Heidenreich <edh5342@psu.edu>
2025-01-23 22:05:04 -0500

    Comments

--- goals_heatmap.py
+++ goals_heatmap.py
@@ -2,13 +2,16 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
+# Gather data from file
 data = pd.read_csv('heatmap_test_data.csv')
 
+# Label rows/columns
 row_labels = ['Top', 'Mid', 'Bottom']
 col_labels = ['L', 'Mid', 'R']
 
-sns.heatmap(data, vmin=0, vmax=50, cmap='Reds', cbar=True, yticklabels=row_labels, xticklabels=col_labels)
+# Create the heatmap
+sns.heatmap(data, vmin=0, vmax=50, cmap='Blues', cbar=True, yticklabels=row_labels, xticklabels=col_labels, annot=True)
 
+# Show heatmap
 plt.show()
     
